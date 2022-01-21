57c57d44205f7209e52561c23f28cf361a0fd3fb
NCsamii <ncsameie@gmail.com>
2022-01-22 02:20:40 +0330

    feat:add header to components

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import cssStyle from "./header.module.css";
+function Header(){
+    return(
+<div className={cssStyle.navbar}>
+<div className={cssStyle.navLogo}>
+
+</div>
+<div className={cssStyle.navMenu}>
+<ul>
+<li></li>
+</ul>
+</div>
+</div>
+    )
+}
+export default Header;
