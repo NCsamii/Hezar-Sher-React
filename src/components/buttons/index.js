c80130e6a19525b4dd46bc832cdac05e641afcd9
NCsamii <ncsameie@gmail.com>
2022-01-22 03:37:58 +0330

    fix:change Ui homePage

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,8 +1,8 @@
 import cssStyle from "./buttons.module.css";
 function Buttons(props){
-const{content , Class , onclick , type , value , name , disable } = props;
+const{content , theme , onclick , type , value , name , disable } = props;
 return(
-<button className={cssStyle[Class]} Class="test" type={type} name={name} onClick={onclick}>{content}</button>
+<button className={cssStyle[theme]}  type={type} name={name} onClick={onclick}>{content}</button>
     )
 }
 export default Buttons;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,47 @@
 import cssStyle from "./home.module.css";
+import Header from "../../components/header";
+import imgLeft from "../../assets/img/vector2-2.png";
+import imageFirts from "../../assets/img/imagefirst.png";
+import Buttons from "../../components/buttons" ;
+import { Link, useNavigate } from "react-router-dom";
 function Home()
 {
+    const navigate = useNavigate();
     return(
-        <span>home</span>
-    )
+<section className={cssStyle.section}>
+<diV className={cssStyle.Header}>
+    <Header />
+</diV>
+<div className={cssStyle.home}>
+    <div className={cssStyle.container}>
+        <div className={cssStyle.vectorLeft}>
+            <img src={imgLeft} className={cssStyle.imgLeft}/>
+        </div>
+        <div className={cssStyle.contents}>
+            <div>
+                <img src={imageFirts} className={cssStyle.contentImage} />
+            </div>
+            <div style={{paddingRight: '24px'}}>
+            <p>
+                            تاریخ کهن ایران زمین با پیشینه ای غنی از فرهنگ و
+                            هنر، از دیرباز با شعر و ادبیات درهم پیچیده و ایران،
+                            مهد پرورش شاعران بزرگی بوده و در گیر و دار فراز و
+                            نشیب هایی که در طول تاریخ بر آن گذشته، اما شاهنامه
+                            وزین پارسی پرور فردوسی بزرگ، اشعار روح بخش مولانا و
+                            عشق بازی اش با حق، غزل های عاشقانه و عارفانه عالم
+                            گیر در دیوان حافظ، اندیشه ژرف و عرفان ناب عطار
+                            نیشابوری همه و همه همچون مرهمی بر زخم های کوچک و
+                            بزرگش تسکین داده اند.
+                        </p>          
+                        
+                        <Buttons theme="yellow" content="فال حافظ"  onclick={() => {navigate('/');
+                                }}  />
+                                                     <Buttons theme="yellow" content="گنجینه اشعار"  onclick={() => {navigate('/');
+                                }}  />
+                          </div>
+        </div>
+    </div>
+</div>
+</section>    )
 }
 export default Home;
