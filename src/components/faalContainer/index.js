12a36b91c2bb346519487e8168b30b74ce1185f7
NCsamii <ncsameie@gmail.com>
2022-01-22 08:45:56 +0330

    feat:add faal component

---
+++ src/components/faalContainer/index.js
@@ -0,0 +1,10 @@
+import cssStyle from "./faalContainer.module.css";
+function FaalContainer(){
+return (
+    <div className={cssStyle.faalContainer}>
+
+        
+    </div>
+)
+}
+export default FaalContainer;
--- src/pages/hafezFaal/index.js
+++ src/pages/hafezFaal/index.js
@@ -1,8 +1,11 @@
+import FaalContainer from "../../components/faalContainer";
 import cssStyle from "./hafezFaal.module.css";
 function HafezFaal(){
 return (
     <section className={cssStyle.faalSection}>
-  
+  <div className={cssStyle.Container}>
+      <FaalContainer />
+  </div>
     </section>
 )
 }
