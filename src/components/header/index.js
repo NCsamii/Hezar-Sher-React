9eec571a1a4167488981d34004d75b9bd2e54c03
NCsamii <ncsameie@gmail.com>
2022-01-22 08:28:40 +0330

    feat:add font to project
    feat:add content to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,25 @@
 import cssStyle from "./header.module.css";
 import { Link } from "react-router-dom";
+import logo from "../../assets/img/logo.png"
 function Header(){
     return(
 <div className={cssStyle.navbar}>
 <div className={cssStyle.navLogo}>
+<a href="/">
+<img src={logo} />
 
+</a>
 </div>
 <div className={cssStyle.navMenu}>
 <ul>
 <li>
+<Link to='/f'>فال حافظ</Link>
+</li>
+<li>
+<Link to='/'>فال حافظ</Link>
+</li>
+<li>
+<Link to='/'>فال حافظ</Link>
 </li>
 </ul>
 </div>
